46db006a27c13e201eff5c6cfb158c3525d8d67b
Abdullah Suhail <abdullahcodes1812@gmail.com>
2026-04-17 20:36:41 +0530

    3761 leetcode

---
+++ 3761,Minimun_absolute_distance.py
@@ -0,0 +1,52 @@
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+
+        l=len(nums)
+
+        # original={}
+
+        reversemap={}
+
+        answer=float('inf')
+
+ 
+        for i in range(0,l):
+
+             
+            reversenum = int(str(nums[i])[::-1])
+
+            if nums[i] in reversemap:
+
+                foundidx=reversemap[nums[i]]
+
+                temp=abs(foundidx-i)
+
+                answer=min(temp,answer)
+
+            else:
+
+                pass
+
+            
+            reversemap[reversenum]=i
+
+
+        if answer==float('inf'):
+
+            answer=-1
+
+
+        return answer
+
+
+
+
+
+        
+
+
+
+
+
+
+        
